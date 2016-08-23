909e8439960f757a0dc91fa8defad75c578a0124
RobertSchouten <robert_schouten@live.com>
2016-08-23 15:30:00 +0800

    move contact match to inbox app

--- frappe/email/__init__.py
+++ frappe/email/__init__.py
@@ -8,23 +8,6 @@ from frappe.email.email_body import get_email
 from frappe.email.smtp import send
 from frappe.utils import markdown
 
-def set_customer_supplier(sender,recipients):
-	origin_contact = frappe.db.sql("select name,email_id,supplier,supplier_name, customer,customer_name,user from `tabContact`",as_dict=1)
-
-	for comm in origin_contact:
-		if comm["user"] is None and comm["email_id"]:
-			if (sender and sender.find(comm["email_id"]) > -1) or (
-				recipients and recipients.find(comm["email_id"]) > -1):
-				if comm["supplier"] and comm["customer"]:
-					return {"timeline_doctype": "Contact","timeline_name":comm["name"],"timeline_label":None}
-
-				elif comm["supplier"]:
-					return {"timeline_doctype": "Supplier","timeline_name":comm["supplier"],"timeline_label":comm["supplier_name"]}
-
-				elif comm["customer"]:
-					return {"timeline_doctype": "Customer", "timeline_name": comm["customer"],"timeline_label": comm["customer_name"]}
-	return {"timeline_doctype": None,"timeline_name":None,"timeline_label":None}
-
 def sendmail_md(recipients, sender=None, msg=None, subject=None, attachments=None, content=None,
 	reply_to=None, cc=(), message_id=None, in_reply_to=None):
 	"""send markdown email"""
